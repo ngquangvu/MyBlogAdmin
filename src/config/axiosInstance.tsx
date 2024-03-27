3bacdba355f6651280cdc5c0cca40b54e542a549
ngquangvu <nguyenquangvu.work@gmail.com>
2024-03-27 21:48:14 +0700

    update refresh token in axios interceptor

--- src/config/axiosInstance.tsx
+++ src/config/axiosInstance.tsx
@@ -7,28 +7,32 @@ const axiosInstance = axios.create({
   withCredentials: true
 })
 
-axiosInstance.interceptors.response.use((response) => {
-  if (response.data?.status === 401 && response.data?.path && !response.data?.path.includes('login')) {
-    return new Promise((resolve, reject) => {
-      axios
-        .get('/admin/authentication/refresh', {
-          baseURL: import.meta.env.VITE_API_URL,
-          timeout: 30000,
-          withCredentials: true
-        })
-        .then(({ data }) => {
-          if (data.status === 401) {
-            removeAdminFromLocalStorage()
-          }
-          location.reload()
-        })
-        .catch((err) => {
-          reject(err)
-          console.log(err)
-        })
-    })
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const { response, config } = error
+    if (response.status === 401 && response.config?.url && !response.config?.url.includes('login')) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get('/admin/authentication/refresh', {
+            baseURL: import.meta.env.VITE_API_URL,
+            timeout: 30000,
+            withCredentials: true
+          })
+          .then(({ data }) => {
+            if (data.statusCode === 401) {
+              removeAdminFromLocalStorage()
+            }
+            location.reload()
+          })
+          .catch((err) => {
+            reject(err)
+            console.log(err)
+          })
+      })
+    }
+    return response
   }
-  return response
-})
+)
 
 export default axiosInstance
